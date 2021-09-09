b0e0d7866f2b54ebc2308c67bce62803150701b6
Ben Mosley <bmosley1132@gmail.com>
2021-09-09 12:34:38 -0400

    implemented string method and event listeners for posts

--- src/scripts/feed/post.js
+++ src/scripts/feed/post.js
@@ -1,11 +1,11 @@
- export const Post = (postObject) => {
-    return `
-      <section class="post">
-        <header>
-            <h2 class="post__title">${postObject.title}</h2>
-        </header>
-        <img class="post__image" src="${postObject.imageURL}" />
-        <div><button id="edit--${postObject.id}">Edit</button></div>
-      </section>
-    `
-  }
+export const Post = (postObject) => {
+  return `
+    <section class="post" id=${postObject.id}>
+      <header>
+          <h2 class="post__title">${postObject.title}</h2>
+      </header>
+      <img class="post__image" src="${postObject.imageURL}" />
+      <div><button id="edit--${postObject.id}">Edit</button></div>
+    </section>
+  `
+}
